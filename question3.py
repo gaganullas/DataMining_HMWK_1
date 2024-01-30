5e0023d44e0eac6b087bd63daf40ca93b2258ad1
gaganullas <121843706+gaganullas@users.noreply.github.com>
2024-01-29 22:40:13 -0500

    Update question3.py

--- question3.py
+++ question3.py
@@ -1,31 +1,31 @@
 def question3_1():
-    answer = ['Discrete', 'Quantitative','Ratio']
+    answer = ['discrete', 'quantitative','ratio']
     return answer
 
 def question3_2():
-    answer = ['Discrete', 'Qualitative','Ordinal']
+    answer = ['discrete', 'qualitative','ordinal']
     return answer
 
 def question3_3():
-    answer = ['Discrete', 'Qualitative','Ordinal']
+    answer = ['discrete', 'qualitative','ordinal']
     return answer 
 
 def question3_4():
-    answer = ['continuous', 'Quantitative','Ratio']
+    answer = ['continuous', 'quantitative','ratio']
     return answer
 
 def question3_5():
-    answer = ['Discrete', 'Qualitative','Nominal']
+    answer = ['discrete', 'qualitative','nominal']
     return answer
 
 def question3_6():
-    answer = ['continuous', 'Quantitative','Ratio']
+    answer = ['continuous', 'quantitative','ratio']
     return answer
 
 def question3_7():
-    answer = ['continuous', 'quantitative','Interval']
+    answer = ['continuous', 'quantitative','interval']
     return answer
 
 def question3_8():
-    answer = ['Discrete', 'Qualitative','Ordinal']
+    answer = ['discrete', 'qualitative','ordinal']
     return answer
