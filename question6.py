797de86f64bd9b111224c678c166fe87ce4104a6
gaganullas <121843706+gaganullas@users.noreply.github.com>
2024-01-29 20:46:29 -0500

    Update question6.py

--- question6.py
+++ question6.py
@@ -1,15 +1,27 @@
 def question6_1():
-    """
+    
     answer = {
-        'equal_width': ['string', integer],
-        'equal_frequency': ['string', integer]
+        'equal_width': ['string', 1],
+        'equal_frequency': ['string', 1]
     }
-    """
+   
     return answer
 
 def question6_2():
+
+    answer = {
+        'equal_width': ['string', 1],
+        'equal_frequency': ['string', 1]
+    }
+   
     return answer
 
 def question6_3():
+
+    answer = {
+        'equal_width': ['string', 1],
+        'equal_frequency': ['string', 1]
+    }
+   
     return answer 
 
