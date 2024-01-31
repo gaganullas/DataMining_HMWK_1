781a9dd7568ac281acb0f390e2d508531e3c6884
gaganullas <121843706+gaganullas@users.noreply.github.com>
2024-01-30 22:41:44 -0500

    Update question9.py

--- question9.py
+++ question9.py
@@ -7,11 +7,11 @@ def question9_2():
     return answer
 
 def question9_3():
-    answer = 'correlation coefficient'
+    answer = 'Euclidean'
     return answer 
 
 def question9_4():
-    answer = 'Euclidean'
+    answer = 'cosine similarity'
     return answer 
 
 def question9_5():
